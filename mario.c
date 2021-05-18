b04a2b5e7c9910cfcb029b6df9af7c3c913dcf6b
arminrm <68967290+arminrm@users.noreply.github.com>
2021-05-17 22:59:18 -0400

    Update mario.c

--- mario.c
+++ mario.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#include <cs50.h>
 
 int main(void)
 {
     int n;
     do
     {
-        n = get_int("Size: ");
+        printf("Size: ");
+        scanf("%d", n);
     }
     while (n < 1 || n > 8);
 
