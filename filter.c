12b7d0e7f0115b79019926125591477691d4e681
arminrm <68967290+arminrm@users.noreply.github.com>
2021-05-25 13:51:53 -0400

    Update filter.c

--- filter.c
+++ filter.c
@@ -137,5 +137,102 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE cimage[height + 2][width + 2];
+
+    int Gx[3][3];
+    int Gy[3][3];
+
+    int Gx_blue = 0;
+    int Gx_green = 0;
+    int Gx_red = 0;
+
+    int Gy_blue = 0;
+    int Gy_red = 0;
+    int Gy_green = 0;
+
+    //copy picture with black borders
+
+    for (int i = 0; i < height + 2; i++) //copy image
+    {
+        for (int j = 0; j < width + 2; j++)
+        {
+            if (i == 0 || i == height + 1 || j == 0 || j == width + 1)
+            {
+                cimage[i][j].rgbtBlue = 0;
+                cimage[i][j].rgbtGreen = 0;
+                cimage[i][j].rgbtRed = 0;
+            }
+            else
+            {
+                cimage[i][j] = image[i - 1][j - 1];
+            }
+        }
+    }
+
+    //create Gx and Gy convolution matrices
+
+    Gx[0][0] = -1;
+    Gx[1][0] = -2;
+    Gx[2][0] = -1;
+
+    Gy[0][0] = -1;
+    Gy[0][1] = -2;
+    Gy[0][2] = -1;
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (j == 1)
+            {
+                Gx[i][j] = 0;
+            }
+            else if (j == 2)
+            {
+                Gx[i][j] = -1 * Gx[i][j - 2];
+            }
+
+            if (i == 1)
+            {
+                Gy[i][j] = 0;
+            }
+            else if (i == 2)
+            {
+                Gy[i][j] = -1 * Gy[i - 2][j];
+            }
+        }
+    }
+
+    //weigted sum
+    for (int i = 0; i < height + 2; i++)
+    {
+        for (int j = 0; j < width + 2; j++)
+        {
+            if (i != 0 && i != height + 1 && j != 0 && j != width + 1)
+            {
+                for (int k = 0; k < 3; k++)
+                {
+                    for (int h = 0; h < 3; h++)
+                    {
+                        Gx_blue = Gx_blue + Gx[k][h] * cimage[i - 1 + k][j - 1 + h].rgbtBlue;
+                        Gx_green = Gx_green + Gx[k][h] * cimage[i - 1 + k][j - 1 +h].rgbtGreen;
+                        Gx_red = Gx_red + Gx[k][h] * cimage[i - 1 + k][j - 1 + h].rgbtRed;
+
+                        Gy_blue = Gy_blue + Gy[k][h] * cimage[i - 1 + k][j - 1 + h].rgbtBlue;
+                        Gy_green = Gy_green + Gy[k][h] * cimage[i - 1 + k][j - 1 +h].rgbtGreen;
+                        Gy_red = Gy_red + Gy[k][h] * cimage[i - 1 + k][j - 1 + h].rgbtRed;
+                    }
+                }
+            }
+
+            if (i > 0 && j > 0 && i < height + 1 && j < width + 1)
+            {
+                image[i - 1][j - 1].rgbtBlue = round((float)sqrt(pow(Gx_blue,2) + pow(Gy_blue,2)));
+                image[i - 1][j - 1].rgbtGreen = round((float)sqrt(pow(Gx_green,2) + pow(Gy_green,2)));
+                image[i - 1][j - 1].rgbtRed = round((float)sqrt(pow(Gx_red,2) + pow(Gy_red,2)));
+            }
+        }
+    }
+
     return;
 }
